586540237bd1c254b7a6535685b392b6163c079f
takumimimi1277 <mitaku.jima@gmail.com>
2025-10-14 13:43:21 +0900

    書き出せるように

---
+++ Source/MINI4WD_Simulator/MINI4WD_Simulator.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MINI4WD_Simulator.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MINI4WD_Simulator, "MINI4WD_Simulator" );
